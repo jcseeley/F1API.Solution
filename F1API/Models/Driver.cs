45b2326c7b40c238b558f36772ca9843f006c3ef
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-31 14:53:05 -0700

    add validations to Driver.cs.

--- F1API/Models/Driver.cs
+++ F1API/Models/Driver.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace F1API.Models
 {
   public class Driver
   {
     public int DriverId { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Name {get; set; }
+    [Required]
+    [Range(0, 150, ErrorMessage = "Age must be between 0 and 150.")]
     public int Age { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Team { get; set; }
   }
 }
